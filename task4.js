e5bbf65ef6854bd971c360d8778e2b996e2c3ad9
korepin <niperok@yandex.ru>
2020-06-22 18:20:48 +0300

    Выполнил задания к уроку №3

--- task4.js
+++ task4.js
@@ -1,46 +1,21 @@
-'use strict'
+'use strict';
 
-let a = +prompt("Введите первое число");
-let b = +prompt("Введите второе число");
+const products = [
+    {
+        id: 3,
+        price: 200
+    },
+    {
+        id: 4,
+        price: 900
+    },
+    {
+        id: 1,
+        price: 1000
+    },
+];
 
-
-/**
- * Функция возвращает сумму двух чисел
- * @param {number} a Первое число
- * @param {number} b Второе число
- */
-function getSum(a, b) {
-    return a + b;
-}
-
-/**
- * Функция возвращает разность двух чисел
- * @param {number} a Первое число
- * @param {number} b Второе число
- */
-function getSub(a, b) {
-    return a - b;
-}
-
-/**
- * Функция возвращает отношение двух чисел
- * @param {number} a Первое число
- * @param {number} b Второе число
- */
-function getDiv(a, b) {
-    return a / b;
-}
-
-/**
- * Функция возвращает произведение двух чисел
- * @param {number} a Первое число
- * @param {number} b Второе число
- */
-function getMul(a, b) {
-    return a * b;
-}
-
-alert("Сумма равна: " + getSum(a, b));
-alert("Разность равна: " + getSub(a, b));
-alert("Отношение равно: " + getDiv(a, b));
-alert("Произведение равно: " + getMul(a, b));
+products.forEach(function (product) {
+    product.price *= 0.85;
+})
+console.log(products);
