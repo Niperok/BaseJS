63291bcb1f36cdf7e86b890cfe82709f067deefb
korepin <niperok@yandex.ru>
2020-07-10 21:52:09 +0300

    Выполнил задание к уроку № 8

--- src/Settings.js
+++ src/Settings.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class Settings {
-    constructor(colsCount = 16, rowsCount = 16, bombsCount = 5) {
+    constructor(colsCount = 16, rowsCount = 16, bombsCount = 20) {
         this.colsCount = colsCount;
         this.rowsCount = rowsCount;
         this.bombsCount = bombsCount;
